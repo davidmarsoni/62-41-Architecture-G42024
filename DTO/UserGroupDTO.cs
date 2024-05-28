db5eb7f045a7d6aec01aac921d99f07cc31187a5
MathiasPitteloud <mathpitt02@gmail.com>
2024-05-28 15:46:56 +0200

    Add UserGroupDTO

---
+++ DTO/UserGroupDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class UserGroupDTO
+    {
+        public int UserId { get; set; }
+        public string? Username { get; set; }
+        public int GroupId { get; set; }
+        public string? GroupName { get; set; }
+        public string? Acronym { get; set; }
+    }
+}
